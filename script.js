ac2431a52d6532efea02354feb66aa859c36cd30
9381741733 <jyothsnabhukya022@gmail.com>
2025-01-05 10:29:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code herefunction indexOfIgnoreCase(str: string, subStr: string): number {
-    // Convert both strings to lowercase for case-insensitive comparison
-    const lowerStr = s1.toLowerCase();
-    const lowerSubStr = s2.toLowerCase();
+function indexOfIgnoreCase(str, subStr) {
+    // Convert both strings to lowercase to make the comparison case-insensitive
+    const lowerStr = str.toLowerCase();
+    const lowerSubStr = subStr.toLowerCase();
 
-    // Use indexOf to find the position of the substring
+    // Use indexOf to find the first occurrence of the substring
     return lowerStr.indexOf(lowerSubStr);
 }
-	
-}
-
-// Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
